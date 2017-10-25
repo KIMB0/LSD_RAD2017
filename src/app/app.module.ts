2aaf2625a689f38676be15588535f861ca91675d
KIMB0 <Kimsen1992@gmail.com>
2017-10-25 15:59:22 +0200

    Added a new component. Also added the start of the navbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HackerNewsComponent } from './hacker-news/hacker-news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HackerNewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'hackernews', component: HackerNewsComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]

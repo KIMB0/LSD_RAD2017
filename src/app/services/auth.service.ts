78a05ca9368f5752ac31becf19fcec60196f78ff
KIMB0 <Kimsen1992@gmail.com>
2017-11-01 22:51:48 +0100

    added 2-way-binding in login html.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  private baseURL = 'http://94.130.57.246:9000/hackernews/';
 
+  constructor(private http: HttpClient) { }
+
+  login(object: Object) {
+    return this.http.post(this.baseURL + 'login/', object);
+  }
 }

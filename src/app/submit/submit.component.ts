4f0aac6f3ebf5db7ad858377267a508f43262cd5
KIMB0 <Kimsen1992@gmail.com>
2017-11-01 19:55:35 +0100

    Added styling to submit page. Also added 2-way-binding ready.

--- src/app/submit/submit.component.ts
+++ src/app/submit/submit.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubmitComponent implements OnInit {
 
+  title: String = '';
+  url: String = '';
+  text: String = '';
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submitItem() {
+    console.log(this.title, this.url, this.text);
   }
 
 }

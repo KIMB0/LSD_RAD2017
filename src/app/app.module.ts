28b4d7ff9abc574a16c36e9a3ea57cc1e83d5630
KIMB0 <Kimsen1992@gmail.com>
2017-11-01 21:36:45 +0100

    Added httpmodule to module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HackerNewsComponent } from './hacker-news/hacker-news.component';
@@ -30,17 +31,18 @@ import { AuthService } from './services/auth.service';
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      {path: 'news', component: HackerNewsComponent},
-      {path: 'newest', component: NewComponent},
-      {path: 'newcomments', component: CommentsComponent},
-      {path: 'show', component: ShowComponent},
-      {path: 'ask', component: AskComponent},
-      {path: 'jobs', component: JobsComponent},
-      {path: 'submit', component: SubmitComponent},
-      {path: 'app-login', component: LoginComponent},
-      {path: '**', redirectTo: 'news', pathMatch: 'full'},
+      { path: 'news', component: HackerNewsComponent },
+      { path: 'newest', component: NewComponent },
+      { path: 'newcomments', component: CommentsComponent },
+      { path: 'show', component: ShowComponent },
+      { path: 'ask', component: AskComponent },
+      { path: 'jobs', component: JobsComponent },
+      { path: 'submit', component: SubmitComponent },
+      { path: 'app-login', component: LoginComponent },
+      { path: '**', redirectTo: 'news', pathMatch: 'full' },
 
     ]),
+    HttpClientModule,
     FormsModule
   ],
   providers: [

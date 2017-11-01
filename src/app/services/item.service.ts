0e9d2b933756db263af2bf9eeb732fa19af610dd
KIMB0 <Kimsen1992@gmail.com>
2017-11-02 00:16:08 +0100

    Added a get method for items in itemservice.

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable()
 export class ItemService {
 
-  constructor() { }
+  private baseURL = 'http://94.130.57.246:9000/hackernews/';
 
+  constructor(private http: HttpClient) { }
+  
+  getItems() {
+    return this.http.get(this.baseURL + 'item/');
+  }
 }

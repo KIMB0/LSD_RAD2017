92eadc40b64f5d9241a0a63e817bb6bf177bc936
KIMB0 <Kimsen1992@gmail.com>
2017-11-17 22:35:30 +0100

    Added new line command

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -5,6 +5,5 @@ set -x # Show the output of the following commands (useful for debugging)
 openssl aes-256-cbc -K $encrypted_394c2ab74843_key -iv $encrypted_394c2ab74843_iv -in deploy_rsa.enc -out deploy_rsa -d
 eval "$(ssh-agent -s)"
 chmod 600 deploy_rsa
-ssh-add deploy_rsa
-echo "\n"
+ssh-add deploy_rsa echo "\n"
 mv deploy_rsa ~/.ssh/id_rsa

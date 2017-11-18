8b340507c0dc6b8ffd15a4b0caf773288367cc62
KIMB0 <Kimsen1992@gmail.com>
2017-11-18 13:59:59 +0100

    Travis deploy pt 2.

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 set -x
-if [ $TRAVIS_BRANCH == 'master' ] ; then
-    # Initialize a new git repo in _site, and push it to our server.
-    cd resources
-    git init
+
+# Initialize a new git repo in _site, and push it to our server.
+git init
         
-    git remote add deploy "root@138.197.42.192:/var/repo/.git-travis"
-    git config user.name "KIMB0"
-    git config user.email "kimsen1992@gmail.com"
+git remote add deploy "root@138.197.42.192:/var/repo/.git-travis"
+git config user.name "KIMB0"
+git config user.email "kimsen1992@gmail.com"
     
-    git add .
-    git commit -m "Deploy"
-    git push --force deploy master
-else
-    echo "Not deploying, since this branch isn't master."
-fi  
+git add .
+git commit -m "Deploy"
+git push --force deploy master

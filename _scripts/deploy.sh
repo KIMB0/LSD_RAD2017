26588c357f23f8df22894519390d2d0215b92988
KIMB0 <Kimsen1992@gmail.com>
2017-11-18 14:17:12 +0100

    Travis deploy pt 4.

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -4,7 +4,7 @@ set -x
 # Initialize a new git repo in _site, and push it to our server.
 git init
 
-git remote add deploy "root@138.197.42.192:/var/repo/git-travis"
+git remote add deploy "root@138.197.42.192:/var/repo/travis.git"
 git config user.name "KIMB0"
 git config user.email "kimsen1992@gmail.com"
 

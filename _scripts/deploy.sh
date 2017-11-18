9cf515eb2ba03ddd2cf85c0cb806db7d045e1526
KIMB0 <Kimsen1992@gmail.com>
2017-11-18 15:06:14 +0100

    Travis deploy pt 9.

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -8,6 +8,7 @@ git remote add deploy "root@138.197.42.192:/var/repo/travis.git"
 git config user.name "KIMB0"
 git config user.email "kimsen1992@gmail.com"
 git branch -a
+git checkout travis-setup
 
 git add .
 git commit -m "Deploy"

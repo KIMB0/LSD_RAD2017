a750861817cd2b7f19549f011191145506df4143
KIMB0 <Kimsen1992@gmail.com>
2018-01-04 16:30:45 +0100

    Added a method which can get the kids and put them in array.

--- src/pages/selected-story/selected-story.component.ts
+++ src/pages/selected-story/selected-story.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ItemService } from '../../services/item.service';
+import { forwardRef } from '@angular/core/src/di/forward_ref';
+import { forEach } from '@angular/router/src/utils/collection';
 
 
 @Component({
@@ -9,11 +12,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SelectedStoryComponent implements OnInit {
   private item: any;
+  private kids: any = [];
 
-  constructor(private router: ActivatedRoute) {
+  constructor(private router: ActivatedRoute, private itemService: ItemService) {
     this.router.params.subscribe(params => {
       this.item = params;
+      if (this.item.kids !== 'null' ) {this.getKids(); }
     });
+  }
+
+  getKids() {
+    const kidsArray = this.item.kids.split(',').map(Number);
+    for (const number of kidsArray) {
+      this.itemService.getItemsById(number).subscribe(response => {
+        this.kids.push(response); console.log(this.kids);
+      });
+    }
   }
 
   ngOnInit() {
